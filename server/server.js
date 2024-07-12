a26622cfa19ae393fa50d93dd231d990eccccf7b
viettranni <vttranviett@gmail.com>
2024-07-13 01:54:22 +0300

    Took bigger steps to connect ot the MongoDB Atlas and inserting the information in JSON to the database from the client side (script.js and html).

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const User = require('../models/User.js')
+
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json())
+
+//------------------------------------------------------------------------------------------------------------------------
+
+// Connecting to MongoDB, taking the key from .env file (MONGODB_URI)
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri)
+    .then(() => console.log('MongoDB Atlas connected successfully!'))
+    .catch(err => console.error('MongoDB Atlas connection error:', err));
+
+//------------------------------------------------------------------------------------------------------------------------
+
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -14,6 +33,27 @@ app.get('/', (req, res) => {
 app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
+
+
+// API own test endpoint to insert data to the DebtDiary database
+app.post('/api/users', (req, res) => {
+    const { name, creditor, amount, currency, dueDate } = req.body;
+    const newUser = new User({
+        name,
+        creditor,
+        amount,
+        currency,
+        dueDate: new Date(dueDate)
+    });
+
+    newUser.save()
+        .then(user => res.json(user))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+app.get('/api/users', (req, res) => {
+
+})
 
 // Start the server
 const PORT = process.env.PORT || 3000;
